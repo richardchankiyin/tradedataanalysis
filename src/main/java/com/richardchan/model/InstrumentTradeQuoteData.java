a8cfab2d11da54e2780819eea5042dd79c260011
Richard Chan <richardchankiyin@gmail.com>
2023-08-15 22:37:00 +0800

    init InstrumentTradeQuoteData

---
+++ src/main/java/com/richardchan/model/InstrumentTradeQuoteData.java
@@ -0,0 +1,12 @@
+package com.richardchan.model;
+
+import java.util.List;
+
+public class InstrumentTradeQuoteData {
+	
+	private String id;
+	private List<Data> tradeData;
+	private List<Data> quoteData;
+	
+
+}
